3e6f61b06981e3a6561fea8461fe2d6a9a93c57f
David Choo <davidchoo16@gmail.com>
2015-09-04 19:07:57 +0800

    added bug report link to issues in github

--- popup.js
+++ popup.js
@@ -1,1 +1,2 @@
 document.getElementById('sourceCodeLink').onclick = function(){chrome.tabs.create({url:"https://github.com/davidchoo12/youtube-focuser-looper"});};
+document.getElementById('bugReportLink').onclick = function(){chrome.tabs.create({url:"https://github.com/davidchoo12/youtube-focuser-looper/issues"});};
